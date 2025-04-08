37bc8e9e52813021c8043369a06d154daf081859
Ulises Lárraga <uliseslarraga@gmail.com>
2025-04-08 08:57:02 -0600

    debug

--- build-support/create-matrix.py
+++ build-support/create-matrix.py
@@ -14,6 +14,7 @@ def get_directories(commit_hash):
     try:
         return subprocess.run(full_command, shell=True, capture_output=True, text=True)
     except subprocess.CalledProcessError:
+        print("Failed to execute command")
         return []
 
 def run():
