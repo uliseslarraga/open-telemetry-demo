b68bb50eb114c25a62b08fbaaf346fd3c30403f4
Ulises Lárraga <uliseslarraga@gmail.com>
2025-04-07 19:01:22 -0600

    feat: Add python script to build docker images on demand

---
+++ build-support/create-matrix.py
@@ -0,0 +1,29 @@
+import json
+import subprocess
+import os
+
+def folder_contains_dockerfile(folder_path):
+    try:
+        return "Dockerfile" in os.listdir(folder_path)
+    except FileNotFoundError:
+        print(f"Folder not found: {folder_path}")
+        return False
+    
+def get_directories():
+    full_command="git diff-tree --no-commit-id --diff-filter=M --name-only -r HEAD~0 | while read f; do dirname \"$f\"; done | sort -u"
+    try:
+        return subprocess.run(full_command, shell=True, capture_output=True, text=True)
+    except subprocess.CalledProcessError:
+        return []
+
+def run():
+    modified_dirs = get_directories().stdout.strip().splitlines()
+    for dir in modified_dirs:
+        if not folder_contains_dockerfile(dir):
+            modified_dirs.remove(dir)
+
+    with open("output.json", "w") as f:
+        json.dump(modified_dirs, f, indent=4)
+
+if __name__ == "__main__":
+    run()
